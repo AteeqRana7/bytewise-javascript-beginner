698cf5e4123575fb2efa4b80ef1ffd8593b3e20e
Ateeq Rana <ateeq.rana98@gmail.com>
2023-03-28 23:26:38 +0500

    variables

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log("hello from external");
+//this is a comment in JS
+//this is a log statement
+// console.log("hello from external");
+
+//variables
+// let age = 23;
+// console.log(age);
+
+// const salary = 500;
+// console.log(salary);
+
+// let name = "Ateeq";
+// const language = "JavaScript";
+// const nationally = `Pakistani`;
+
+// const age = 21
+// const salary = 20.4
+
+// const isEligible = true;
+// const isNew = false;
+
+// let result;
+// console.log(result);
+
+// const res = undefined;
+
+// const data = null;
